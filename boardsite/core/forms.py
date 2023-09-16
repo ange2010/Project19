3c960968a65a50807c27f780d998dbb69df43401
ange2010 <112875401+ange2010@users.noreply.github.com>
2023-09-16 16:18:59 +0300

    Initial commit

---
+++ boardsite/core/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Ваше имя',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Ваш пароль',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Ваше имя',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Ваша электронная почта',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Ваш пароль',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Повторите ваш пароль',
+        'class': 'w-fullpy-4 px-6 rounded-xl'
+    }))
